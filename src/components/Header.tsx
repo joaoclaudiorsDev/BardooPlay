24079fc9edda8e52f6228214ffdff452956935e2
João Cláudio <joaoclaudiobio@gmail.com>
2023-06-22 11:45:05 -0300

    4.1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="nav-bar">
+    <header className="nav-bar" data-testid="header-component">
       <nav className="nav">
-        <NavLink to="/search">Search </NavLink>
+        <NavLink data-testid="link-to-search" to="/search">Search </NavLink>
+        <NavLink data-testid="link-to-favorites" to="/favorites">Favorites </NavLink>
+        <NavLink data-testid="link-to-profile" to="/profile">Profile </NavLink>
       </nav>
     </header>
   );
